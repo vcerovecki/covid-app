359bd5982e4ad42236fd2ccef5feace8f6cc89b9
Vinko <vin.cerovecki@gmail.com>
2020-08-02 22:50:27 +0200

    ssl

--- src/api/utils/axiosInstance.js
+++ src/api/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://164.90.223.110:43210",
+  baseURL: "https://164.90.223.110:43210",
 });
 
 export default axiosInstance;
